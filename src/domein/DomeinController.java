cdab66d28603881c38f63ee82cd94f1bb1fdb9ec
jvsHoGent <johan.vanschoor@hogent.be>
2015-02-09 00:50:39 +0100

    implementatie domeincontroller frame

---
+++ src/domein/DomeinController.java
@@ -0,0 +1,9 @@
+package domein;
+
+public class DomeinController {
+
+	public void doingSomeThing() {
+		// TODO - implement DomeinController.doingSomeThing
+		throw new UnsupportedOperationException();
+	}
+}
